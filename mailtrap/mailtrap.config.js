841afba2e54eee01537a164a08ff1a4f3061b346
Kathirvel-123 <kathirvelmuruganwanthan@gmail.com>
2026-04-11 10:49:48 +0530

    updated config.js

--- mailtrap/mailtrap.config.js
+++ mailtrap/mailtrap.config.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
