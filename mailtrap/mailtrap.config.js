2ffa96a9f102a402a0e5e29ac871a4a7939dd10d
Kathirvel-123 <kathirvelmuruganwanthan@gmail.com>
2026-04-11 11:08:14 +0530

    switch to resend

--- mailtrap/mailtrap.config.js
+++ mailtrap/mailtrap.config.js
@@ -1,19 +1,10 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+export const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sender = {
-    email: process.env.EMAIL_USER,
+    email: "onboarding@resend.dev",
     name: "Auth App",
 };
